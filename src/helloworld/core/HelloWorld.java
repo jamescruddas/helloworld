1524229b947d3133b3fae56f79f28c0ab90dc4ed
jamescruddas <cruddasj@hotmail.co.uk>
2016-03-25 14:19:10 +0000

    Initial HelloWorld commit
    First commit for Java HelloWorld test application

---
+++ src/helloworld/core/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package helloworld.core;
+
+/**
+ * @author James Cruddas
+ * Application entry method
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args Default args array
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
