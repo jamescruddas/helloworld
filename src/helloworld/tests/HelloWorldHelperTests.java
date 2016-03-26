33dd128591c2a2703f83bcf787ff60f6a7c55733
jamescruddas <cruddasj@hotmail.co.uk>
2016-03-26 15:59:49 +0000

    Fixed test to show in Jenkins

--- src/helloworld/tests/HelloWorldHelperTests.java
+++ src/helloworld/tests/HelloWorldHelperTests.java
@@ -19,7 +19,6 @@ public class HelloWorldHelperTests {
 	public void HelloWorldHelperReturnsSameString() {
 		String testString = "Hello Jenkins";
 		String helperReturnedString = helloWorldHelper.SaySomething(testString);
-		helperReturnedString += "!";
 		assertEquals(testString, helperReturnedString);
 	}
 }
